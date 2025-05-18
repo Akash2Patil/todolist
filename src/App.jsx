5b94c9992454933ec03bede680d5ac35366ee3ed
Akash2Patil <akashpatil1321999@gmail.com>
2025-05-18 13:50:02 +0530

    components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Addtask from './component/Addtask'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-zinc-500'>
-      App
+    <div className='w-full h-screen bg-[#f8f8ff] flex flex-col items-center p-[6%]'>
+      <h1 className='text-5xl text-zinc-700 font-bold mb-10'>To Do list</h1>
+      <Addtask/>
     </div>
   )
 }
