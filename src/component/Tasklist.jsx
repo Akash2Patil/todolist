25f6f4a5459034e25833066143e2101ac3be96ae
Akash2Patil <akashpatil1321999@gmail.com>
2025-05-25 08:02:58 +0530

    checkbox added

--- src/component/Tasklist.jsx
+++ src/component/Tasklist.jsx
@@ -5,7 +5,7 @@ const Tasklist = ({tasks}) => {
   return (
     <div className='mt-5 w-1/3 bg-[#ecedf6] rounded-lg'>
     <ul>
-      {tasks.map((item,index)=>(<li className='bg-white p-3 rounded-sm m-5' key={index}><p className='w-full break-words'>{item}</p></li>))}
+      {tasks.map((item,index)=>(<li className='bg-white p-3 rounded-sm m-5 flex gap-3 items-center' key={index}> <input type="checkbox" name="" id="" /> <p className='w-full break-words'>{item}</p><button>Edit</button></li>))}
     </ul>
     </div>
   )
