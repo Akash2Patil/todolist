c904f0aaca1ea2e7f26c18ca7d193884c3f582ec
Akash2Patil <akashpatil1321999@gmail.com>
2025-05-25 10:50:49 +0530

    checkbox logic is added

--- src/component/Addtask.jsx
+++ src/component/Addtask.jsx
@@ -8,8 +8,20 @@ const Addtask = () => {
     const handelSubmit = (e) => {
         e.preventDefault();
         const value = taskin.current.value;
-        value ? settask([...task, value]) : alert("Please enter the task"); // this avoid the blank task
-        taskin.current.value = "";
+        if(value){
+            const newtask = {
+                id:Date.now(),
+                text:value,
+                completed:false,
+            };
+            settask([...task,newtask])
+            taskin.current.value = "";
+            console.log(newtask);
+            
+        }
+        else{
+            alert("please add the task")
+        }
     }
     console.log(task);
 
@@ -17,11 +29,11 @@ const Addtask = () => {
         <>
             <div className='border border-[#c7c6c6] rounded-lg w-1/3 p-5'>
                 <form action="" onSubmit={handelSubmit} className='flex gap-3 justify-center'>
-                    <input className='border p-3 w-full rounded-sm' ref={taskin} type="text" placeholder='Add Task' />
-                    <input className='bg-[#646ff0] text-white font-semibold p-3 rounded-sm cursor-pointer' type="submit" />
+                    <input className='border p-3 w-full rounded-sm' ref={taskin} type="text" />
+                    <input className='bg-[#646ff0] text-white font-semibold p-3 rounded-sm cursor-pointer' type="submit" value="Add Task"/>
                 </form>
             </div>
-            <Tasklist tasks={task} />
+            <Tasklist tasks={task} settask={settask}/>
         </>
     )
 }
