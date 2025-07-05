4ce9b6bde0d33805e19e29d1b194309d7d4ad8c2
Akash2Patil <akashpatil1321999@gmail.com>
2025-07-05 19:08:10 +0530

    localstorage connection added

--- src/component/Addtask.jsx
+++ src/component/Addtask.jsx
@@ -1,41 +1,53 @@
-import React, { useRef, useState } from 'react'
-import Tasklist from './Tasklist'
+import React, { useRef, useState, useEffect } from "react";
+import Tasklist from "./Tasklist";
 
 const Addtask = () => {
-    const taskin = useRef(null)
-    const [task, settask] = useState([])
+  const taskin = useRef(null);
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("todo-list");
+    return saved ? JSON.parse(saved) : [];
+  });
 
-    const handelSubmit = (e) => {
-        e.preventDefault();
-        const value = taskin.current.value;
-        if(value){
-            const newtask = {
-                id:Date.now(),
-                text:value,
-                completed:false,
-            };
-            settask([...task,newtask])
-            taskin.current.value = "";
-            console.log(newtask);
-            
-        }
-        else{
-            alert("please add the task")
-        }
+  useEffect(() => {
+    localStorage.setItem("todo-list", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = taskin.current.value.trim();
+    if (value) {
+      const newTask = {
+        id: Date.now(),
+        text: value,
+        completed: false,
+      };
+      setTasks([...tasks, newTask]);
+      taskin.current.value = "";
+    } else {
+      alert("Please add the task");
     }
-    console.log(task);
+  };
 
-    return (
-        <>
-            <div className='border border-[#c7c6c6] rounded-lg w-1/3 p-5'>
-                <form action="" onSubmit={handelSubmit} className='flex gap-3 justify-center'>
-                    <input className='border p-3 w-full rounded-sm' ref={taskin} type="text" />
-                    <input className='bg-[#646ff0] text-white font-semibold p-3 rounded-sm cursor-pointer' type="submit" value="Add Task"/>
-                </form>
-            </div>
-            <Tasklist tasks={task} settask={settask}/>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="border border-[#c7c6c6] rounded-lg w-1/3 p-5">
+        <form onSubmit={handleSubmit} className="flex gap-3 justify-center">
+          <input
+            className="border p-3 w-full rounded-sm"
+            ref={taskin}
+            type="text"
+          />
+          <input
+            className="bg-[#646ff0] text-white font-semibold p-3 rounded-sm cursor-pointer"
+            type="submit"
+            value="Add Task"
+          />
+        </form>
+      </div>
 
-export default Addtask
+      <Tasklist tasks={tasks} settask={setTasks} />
+    </>
+  );
+};
+
+export default Addtask;
