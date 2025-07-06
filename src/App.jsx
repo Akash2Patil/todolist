18432eddf52efb1e9fe0c9fd6ae8d9194f14e777
Akash2Patil <akashpatil1321999@gmail.com>
2025-07-06 17:18:14 +0530

    minor fix in style

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Addtask from './component/Addtask'
 
 const App = () => {
   return (
-    <div className='w-full h-screen bg-[#f8f8ff] flex flex-col items-center p-[6%]'>
+    <div className='w-full h-screen bg-[#f8f8ff] flex flex-col items-center pt-10'>
       <h1 className='text-5xl text-zinc-700 font-bold mb-10'>ToDo List</h1>
       <Addtask/>
     </div>
